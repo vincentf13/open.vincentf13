0a9f0c2c37a6fd2edcf5cf079883cc2a004b54b9
Victor <c.p.kevinf13@gmail.com>
2025-09-30 07:38:21 +0800

    查詢版本號與BUILD時間 接口

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
 import io.micrometer.core.annotation.Timed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+
+    @Value("${APP_IMAGE_TAG:unknown}")
+    String tag;
+
+    private final BuildProperties buildProperties;
+
+    public DemoApplication(BuildProperties buildProperties) {
+        this.buildProperties = buildProperties;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
@@ -15,14 +27,19 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Spring Boot in K8s!";
+
+        return "Hello Spring Boot in K8s! " +
+                "image.tag=" + tag +
+                "Build Time: " + buildProperties.getTime();
+
     }
 
     @GetMapping("/burn")
     @Timed
     public String burn(@RequestParam(defaultValue = "200") int ms) {
         long start = System.currentTimeMillis();
-        while (System.currentTimeMillis() - start < ms) { }
+        while (System.currentTimeMillis() - start < ms) {
+        }
         return "burn " + ms + "ms";
     }
 }
