737d7dab4f423b1adc3d109e6a58f4cd45e7b09c
Victor <c.p.kevinf13@gmail.com>
2025-10-01 20:50:49 +0800

    mvn plugin 調整

--- services/demo/src/main/java/com/example/exchange/demo/DemoApplication.java
+++ services/demo/src/main/java/com/example/exchange/demo/DemoApplication.java
@@ -25,7 +25,7 @@ public class DemoApplication {
     public String hello() {
         return "Hello from 微服務版 Demo !" +
                 "<br/>image.tag =  " + buildProperties.get("image.tag") +
-                "<br/>Build Time: " + buildProperties.get("build.timestamp");
+                "<br/>Build Tim e: " + buildProperties.get("build.timestamp");
     }
 
     @GetMapping("/burn")
