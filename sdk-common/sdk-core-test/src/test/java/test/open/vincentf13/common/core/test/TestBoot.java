6c58fb53c0a615bf3329270ddb4b4bf1ab7c71f2
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-04 23:29:44 +0800

    Kafka OK ， 全測試一起執行又報錯了?

--- sdk-common/sdk-core-test/src/test/java/test/open/vincentf13/common/core/test/TestBoot.java
+++ sdk-common/sdk-core-test/src/test/java/test/open/vincentf13/common/core/test/TestBoot.java
@@ -3,9 +3,11 @@ package test.open.vincentf13.common.core.test;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootConfiguration
 @ComponentScan("open.vincentf13")  // 指定主程式路徑
 @EnableAutoConfiguration
+@Import( {KafkaTest.TestConfig.class})
 public class TestBoot {
 }
