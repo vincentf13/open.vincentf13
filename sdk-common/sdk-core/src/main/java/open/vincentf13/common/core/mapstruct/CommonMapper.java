18066e87a639f7c76b867d3aa2130773291736a2
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-05 21:37:17 +0800

    lombok 與 mapstruct 整合

--- sdk-common/sdk-core/src/main/java/open/vincentf13/common/core/mapstruct/CommonMapper.java
+++ sdk-common/sdk-core/src/main/java/open/vincentf13/common/core/mapstruct/CommonMapper.java
@@ -1,23 +1,23 @@
-package open.vincentf13.common.core.mapstruct;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.TargetType;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@Mapper(componentModel = "spring")
-public interface CommonMapper {
-
-    /*
-     * Usage example:
-     * @Autowired CommonMapper mapper;
-     *
-     * UserDto dto = mapper.map(user, UserDto.class);
-     * List<UserDto> list = mapper.mapList(users, UserDto.class);
-     */
-    <S, T> T map(S source, @TargetType Class<T> targetType);
-
-    <S, T> List<T> mapList(List<S> source, @TargetType Class<T> targetType);
-}
+//package open.vincentf13.common.core.mapstruct;
+//
+//import org.mapstruct.Mapper;
+//import org.mapstruct.TargetType;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@Mapper(componentModel = "spring")
+//public interface CommonMapper {
+//
+//    /*
+//     * Usage example:
+//     * @Autowired CommonMapper mapper;
+//     *
+//     * UserDto dto = mapper.map(user, UserDto.class);
+//     * List<UserDto> list = mapper.mapList(users, UserDto.class);
+//     */
+//    <S, T> T map(S source, @TargetType Class<T> targetType);
+//
+//    <S, T> List<T> mapList(List<S> source, @TargetType Class<T> targetType);
+//}
