4c149f550eb033441d0142c72b8c3af91c346357
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-05 16:11:08 +0800

    doc

--- sdk-common/sdk-core/src/main/java/open/vincentf13/common/core/exception/ServiceException.java
+++ sdk-common/sdk-core/src/main/java/open/vincentf13/common/core/exception/ServiceException.java
@@ -2,8 +2,23 @@ package open.vincentf13.common.core.exception;
 
 import open.vincentf13.common.core.exception.contractor.BaseCheckedException;
 
+import java.util.Map;
+
 public class ServiceException extends BaseCheckedException {
+
     public ServiceException(ErrorCode error, String errorMessage) {
         super(error, errorMessage);
+    }
+
+    public ServiceException(ErrorCode error, String errorMessage, Throwable rootCause) {
+        super(error, errorMessage, rootCause);
+    }
+
+    public ServiceException(ErrorCode error, String errorMessage, Map<String, Object> meta) {
+        super(error, errorMessage, meta);
+    }
+
+    public ServiceException(ErrorCode error, String errorMessage, Map<String, Object> meta, Throwable rootCause) {
+        super(error, errorMessage, meta, rootCause);
     }
 }
