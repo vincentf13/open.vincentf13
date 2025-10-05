d192d6a55bef008fe9f22a94dd5bb13abfdb3f6f
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-05 15:57:01 +0800

    doc

--- sdk-common/sdk-core/src/main/java/open/vincentf13/common/core/exception/ServiceException.java
+++ sdk-common/sdk-core/src/main/java/open/vincentf13/common/core/exception/ServiceException.java
@@ -1,8 +1,8 @@
 package open.vincentf13.common.core.exception;
 
-import open.vincentf13.common.core.exception.contractor.BaseRuntimeException;
+import open.vincentf13.common.core.exception.contractor.BaseCheckedException;
 
-public class ServiceException extends BaseRuntimeException {
+public class ServiceException extends BaseCheckedException {
     public ServiceException(ErrorCode error, String errorMessage) {
         super(error, errorMessage);
     }
