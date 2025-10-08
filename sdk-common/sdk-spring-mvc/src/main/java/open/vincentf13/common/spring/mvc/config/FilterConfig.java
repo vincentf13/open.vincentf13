020735398a8a4de4f79aac9b68192577f0ab0ccb
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-08 19:24:25 +0800

    filter interceptor 配置OK

--- sdk-common/sdk-spring-mvc/src/main/java/open/vincentf13/common/spring/mvc/config/FilterConfig.java
+++ sdk-common/sdk-spring-mvc/src/main/java/open/vincentf13/common/spring/mvc/config/FilterConfig.java
@@ -1,10 +1,8 @@
 package open.vincentf13.common.spring.mvc.config;
 
-import open.vincentf13.common.spring.mvc.filter.RequestCorrelationFilter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.FormContentFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -24,24 +22,24 @@ public class FilterConfig {
     FormContentFilter formContentFilter() {
         return new FormContentFilter();
     }
-
-    /**
-     * 建立 RequestCorrelationFilter，負責補足 traceId/requestId 與同步至 MDC。
-     */
-    @Bean
-    public RequestCorrelationFilter requestCorrelationFilter(MvcProperties properties) {
-        return new RequestCorrelationFilter(properties.getRequest());
-    }
-
-    /**
-     * 將 RequestCorrelationFilter 以指定順序註冊到 Servlet Filter Chain。
-     */
-    @Bean
-    public FilterRegistrationBean<RequestCorrelationFilter> requestCorrelationFilterRegistration(RequestCorrelationFilter filter,
-                                                                                                 MvcProperties properties) {
-        FilterRegistrationBean<RequestCorrelationFilter> registration = new FilterRegistrationBean<>(filter);
-        registration.setOrder(properties.getRequest().getFilterOrder());
-        registration.addUrlPatterns("/*");
-        return registration;
-    }
+//
+//    /**
+//     * 建立 RequestCorrelationFilter，負責補足 traceId/requestId 與同步至 MDC。
+//     */
+//    @Bean
+//    public RequestCorrelationFilter requestCorrelationFilter(MvcProperties properties) {
+//        return new RequestCorrelationFilter(properties.getRequest());
+//    }
+//
+//    /**
+//     * 將 RequestCorrelationFilter 以指定順序註冊到 Servlet Filter Chain。
+//     */
+//    @Bean
+//    public FilterRegistrationBean<RequestCorrelationFilter> requestCorrelationFilterRegistration(RequestCorrelationFilter filter,
+//                                                                                                 MvcProperties properties) {
+//        FilterRegistrationBean<RequestCorrelationFilter> registration = new FilterRegistrationBean<>(filter);
+//        registration.setOrder(properties.getRequest().getFilterOrder());
+//        registration.addUrlPatterns("/*");
+//        return registration;
+//    }
 }
