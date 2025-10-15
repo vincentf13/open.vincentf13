1cee6e5c12b63dbac98be8eb1be465d194e70935
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-15 11:49:48 +0800

    fix

--- sdk-common/sdk-core-test/src/test/java/test/open/vincentf13/common/core/test/TestBoot.java
+++ sdk-common/sdk-core-test/src/test/java/test/open/vincentf13/common/core/test/TestBoot.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 // 單一共用的 Spring Boot 測試主程式；其他測試若需額外 bean，請使用 @TestConfiguration 避免產生多個 @SpringBootConfiguration
 @SpringBootConfiguration
-@ComponentScan("open.vincentf13")  // 指定主程式路徑
+@ComponentScan({"open.vincentf13", "test.open.vincentf13"}) // 掃描正式與測試用元件
 @EnableAutoConfiguration
-public class TestBoot{
+public class TestBoot {
 }
