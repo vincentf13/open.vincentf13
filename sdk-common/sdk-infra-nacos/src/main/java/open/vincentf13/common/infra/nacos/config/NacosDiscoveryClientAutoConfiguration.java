14313f66156bcc290671fe39baabee2caec8c64b
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-19 09:13:50 +0800

    自動配置

---
+++ sdk-common/sdk-infra-nacos/src/main/java/open/vincentf13/common/infra/nacos/config/NacosDiscoveryClientAutoConfiguration.java
@@ -0,0 +1,11 @@
+package open.vincentf13.common.infra.nacos.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@AutoConfiguration
+@ConditionalOnClass(EnableDiscoveryClient.class)
+@EnableDiscoveryClient
+public class NacosDiscoveryClientAutoConfiguration {
+}
