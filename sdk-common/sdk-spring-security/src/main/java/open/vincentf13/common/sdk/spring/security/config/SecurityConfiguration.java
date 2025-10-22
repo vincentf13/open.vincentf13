6f58654e5b0a8da3a3b5b6dda1ab9a1d52836fa1
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-22 10:49:05 +0800

    security

---
+++ sdk-common/sdk-spring-security/src/main/java/open/vincentf13/common/sdk/spring/security/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package open.vincentf13.common.sdk.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+            .formLogin(form -> form.loginPage("/login").permitAll())
+            .logout(logout -> logout.logoutSuccessUrl("/login?logout"));
+        return http.build();
+    }
+}
