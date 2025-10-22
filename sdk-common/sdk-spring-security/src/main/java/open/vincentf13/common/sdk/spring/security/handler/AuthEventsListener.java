d7e95baefcd25cc7266f3ad4455ab0126995c154
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-22 11:39:29 +0800

    AuthEventsListener

---
+++ sdk-common/sdk-spring-security/src/main/java/open/vincentf13/common/sdk/spring/security/handler/AuthEventsListener.java
@@ -0,0 +1,17 @@
+package open.vincentf13.common.sdk.spring.security.handler;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.security.authentication.event.AuthenticationFailureLockedEvent;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthEventsListener {
+  @EventListener
+  public void onFailure(AuthenticationFailureBadCredentialsEvent e){ /* 累計失敗 */ }
+  @EventListener
+  public void onLocked(AuthenticationFailureLockedEvent e){ /* 被鎖定 */ }
+  @EventListener
+  public void onSuccess(AuthenticationSuccessEvent e){ /* 清空失敗次數 */ }
+}
