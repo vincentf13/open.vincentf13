91734a8fa7ba5ea3331c10f1d808e66d1fa9002a
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-30 20:12:31 +0800

    調整

---
+++ service/exchange/exchange-auth/src/main/java/open/vincentf13/exchange/auth/ExchangeAuthApplication.java
@@ -0,0 +1,12 @@
+package open.vincentf13.exchange.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeAuthApplication.class, args);
+    }
+}
---
+++ service/exchange/exchange-user/src/main/java/open/vincentf13/exchange/user/ExchangeUserApplication.java
@@ -0,0 +1,12 @@
+package open.vincentf13.exchange.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeUserApplication.class, args);
+    }
+}
