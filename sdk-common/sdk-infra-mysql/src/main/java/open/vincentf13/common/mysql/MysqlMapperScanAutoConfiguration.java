46e810b3514929054596b5eea25333fc45628145
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-31 15:45:14 +0800

    優化sdk

---
+++ sdk-common/sdk-infra-mysql/src/main/java/open/vincentf13/common/mysql/MysqlMapperScanAutoConfiguration.java
@@ -0,0 +1,14 @@
+package open.vincentf13.common.mysql;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+
+/**
+ * 自動配置 MyBatis Mapper 掃描，避免各服務重複宣告。
+ */
+@AutoConfiguration
+@ConditionalOnClass(MapperScan.class)
+@MapperScan(basePackages = "open.vincentf13")
+public class MysqlMapperScanAutoConfiguration {
+}
