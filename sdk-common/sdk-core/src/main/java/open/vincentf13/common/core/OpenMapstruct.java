ee7de700788e45a4a0022378bbffa1b1bb2a247f
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-31 14:13:05 +0800

    common mapper

--- sdk-common/sdk-core/src/main/java/open/vincentf13/common/core/OpenMapstruct.java
+++ sdk-common/sdk-core/src/main/java/open/vincentf13/common/core/OpenMapstruct.java
@@ -1,23 +1,23 @@
-//package open.vincentf13.common.core;
-//
-//import org.mapstruct.Mapper;
-//import org.mapstruct.TargetType;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//@Mapper(componentModel = "spring")
-//public interface OpenMapstruct {
-//
-//    /*
-//     * Usage example:
-//     * @Autowired CommonMapper mapper;
-//     *
-//     * UserDto dto = mapper.map(user, UserDto.class);
-//     * List<UserDto> list = mapper.mapList(users, UserDto.class);
-//     */
-//    <S, T> T map(S source, @TargetType Class<T> targetType);
-//
-//    <S, T> List<T> mapList(List<S> source, @TargetType Class<T> targetType);
-//}
+package open.vincentf13.common.core;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.TargetType;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Mapper(componentModel = "spring") // 讓 MapStruct 生成的實作類自動註冊為 Spring Bean。
+public interface OpenMapstruct {
+
+    /*
+     * Usage example:
+     * @Autowired CommonMapper mapper;
+     *
+     * UserDto dto = mapper.map(user, UserDto.class);
+     * List<UserDto> list = mapper.mapList(users, UserDto.class);
+     */
+    <S, T> T map(S source, @TargetType Class<T> targetType);
+
+    <S, T> List<T> mapList(List<S> source, @TargetType Class<T> targetType);
+}
