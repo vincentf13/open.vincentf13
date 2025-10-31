63ff218ed623cb0cc073c1cc4b3273ad6b261f30
vincentf13 <c.p.kevinf13@gmail.com>
2025-10-31 20:37:49 +0800

    簡化

--- service/exchange/exchange-user/src/main/java/open/vincentf13/exchange/user/infra/persistence/repository/AuthCredentialRepositoryImpl.java
+++ service/exchange/exchange-user/src/main/java/open/vincentf13/exchange/user/infra/persistence/repository/AuthCredentialRepositoryImpl.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
-public class AuthCredentialMyBatisRepository implements AuthCredentialRepository {
+public class AuthCredentialRepositoryImpl implements AuthCredentialRepository {
 
     private final AuthCredentialMapper mapper;
 
