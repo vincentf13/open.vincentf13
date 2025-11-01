f28f79f7a52290be34d820b70aa5a81e5af4d8db
vincentf13 <c.p.kevinf13@gmail.com>
2025-11-02 05:55:18 +0800

    調整

--- sdk-contract/exchange-sdk/exchange-auth-sdk/exchange-auth-sdk-rest-api/src/main/java/open/vincentf13/exchange/auth/sdk/rest/api/AuthCredentialApi.java
+++ sdk-contract/exchange-sdk/exchange-auth-sdk/exchange-auth-sdk-rest-api/src/main/java/open/vincentf13/exchange/auth/sdk/rest/api/AuthCredentialApi.java
@@ -21,6 +21,7 @@ public interface AuthCredentialApi {
     OpenApiResponse<AuthCredentialPrepareResponse> prepare(@Valid @RequestBody AuthCredentialPrepareRequest request);
 
     @PostMapping
+    @PrivateAPI
     OpenApiResponse<AuthCredentialResponse> create(@Valid @RequestBody AuthCredentialCreateRequest request);
 
 }
--- sdk-contract/exchange-sdk/exchange-user-sdk/exchange-user-sdk-rest-api/src/main/java/open/vincentf13/exchange/user/sdk/rest/api/UserApi.java
+++ sdk-contract/exchange-sdk/exchange-user-sdk/exchange-user-sdk-rest-api/src/main/java/open/vincentf13/exchange/user/sdk/rest/api/UserApi.java
@@ -1,9 +1,12 @@
 package open.vincentf13.exchange.user.sdk.rest.api;
 
 import jakarta.validation.Valid;
-import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
 import open.vincentf13.exchange.user.sdk.rest.api.dto.UserRegisterRequest;
 import open.vincentf13.exchange.user.sdk.rest.api.dto.UserResponse;
+import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
+import open.vincentf13.sdk.spring.security.auth.Jwt;
+import open.vincentf13.sdk.spring.security.auth.PrivateAPI;
+import open.vincentf13.sdk.spring.security.auth.PublicAPI;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,12 +15,15 @@ import org.springframework.web.bind.annotation.*;
 public interface UserApi {
 
     @PostMapping
+    @PublicAPI
     OpenApiResponse<UserResponse> register(@Valid @RequestBody UserRegisterRequest request);
 
     @GetMapping("/me")
+    @Jwt
     OpenApiResponse<UserResponse> getMe();
 
     @GetMapping("/by-email")
+    @PrivateAPI
     OpenApiResponse<UserResponse> findByEmail(@RequestParam("email") String email);
 
 }
