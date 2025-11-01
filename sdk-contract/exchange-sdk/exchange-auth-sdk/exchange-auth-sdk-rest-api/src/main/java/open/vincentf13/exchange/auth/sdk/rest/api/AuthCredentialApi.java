1687badf79750319ac9cd60eff9707e3ce3017ec
vincentf13 <c.p.kevinf13@gmail.com>
2025-11-02 05:52:36 +0800

    no message

--- sdk-contract/exchange-sdk/exchange-auth-sdk/exchange-auth-sdk-rest-api/src/main/java/open/vincentf13/exchange/auth/sdk/rest/api/AuthCredentialApi.java
+++ sdk-contract/exchange-sdk/exchange-auth-sdk/exchange-auth-sdk-rest-api/src/main/java/open/vincentf13/exchange/auth/sdk/rest/api/AuthCredentialApi.java
@@ -1,11 +1,12 @@
 package open.vincentf13.exchange.auth.sdk.rest.api;
 
 import jakarta.validation.Valid;
-import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
 import open.vincentf13.exchange.auth.sdk.rest.api.dto.AuthCredentialCreateRequest;
 import open.vincentf13.exchange.auth.sdk.rest.api.dto.AuthCredentialPrepareRequest;
 import open.vincentf13.exchange.auth.sdk.rest.api.dto.AuthCredentialPrepareResponse;
 import open.vincentf13.exchange.auth.sdk.rest.api.dto.AuthCredentialResponse;
+import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
+import open.vincentf13.sdk.spring.security.auth.PrivateAPI;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public interface AuthCredentialApi {
 
     @PostMapping("/prepare")
+    @PrivateAPI
     OpenApiResponse<AuthCredentialPrepareResponse> prepare(@Valid @RequestBody AuthCredentialPrepareRequest request);
 
     @PostMapping
