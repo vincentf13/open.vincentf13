093e27b60ec74c37aa3c0de53eff282a0b5a77cd
vincentf13 <c.p.kevinf13@gmail.com>
2025-11-12 03:36:20 +0800

    worker id

--- sdk/sdk-core/src/main/java/open/vincentf13/sdk/core/id/IdGeneratorAutoConfiguration.java
+++ sdk/sdk-core/src/main/java/open/vincentf13/sdk/core/id/IdGeneratorAutoConfiguration.java
@@ -2,17 +2,21 @@ package open.vincentf13.sdk.core.id;
 
 import com.github.yitter.contract.IdGeneratorOptions;
 import com.github.yitter.idgen.DefaultIdGenerator;
+import lombok.extern.slf4j.Slf4j;
 import open.vincentf13.sdk.core.config.SnowflakeIdProperties;
+import open.vincentf13.sdk.core.log.OpenLog;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(SnowflakeIdProperties.class)
+@Slf4j
 public class IdGeneratorAutoConfiguration {
 
     @Bean
     public DefaultIdGenerator snowflakeIdGenerator(SnowflakeIdProperties snowflakeIdProperties) {
+        OpenLog.info(log,"Bean Config","DefaultIdGenerator","worker id" , snowflakeIdProperties.getWorkerId());
         IdGeneratorOptions options = new IdGeneratorOptions(snowflakeIdProperties.getWorkerId());
         return new DefaultIdGenerator(options);
     }
