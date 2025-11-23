c8d11e9844cc06d3ae5664b5d0ab45d3263d9675
vincentf13 <c.p.kevinf13@gmail.com>
2025-11-23 20:37:29 +0800

    調整

--- sdk-contract/exchange-sdk/exchange-auth-sdk/exchange-auth-sdk-rest-api/src/main/java/open/vincentf13/exchange/auth/sdk/rest/api/dto/AuthCredentialCreateRequest.java
+++ sdk-contract/exchange-sdk/exchange-auth-sdk/exchange-auth-sdk-rest-api/src/main/java/open/vincentf13/exchange/auth/sdk/rest/api/dto/AuthCredentialCreateRequest.java
@@ -3,6 +3,7 @@ package open.vincentf13.exchange.auth.sdk.rest.api.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import open.vincentf13.exchange.auth.sdk.rest.api.enums.AuthCredentialType;
 
 public record AuthCredentialCreateRequest(
         @NotNull(message = "User id is required")
