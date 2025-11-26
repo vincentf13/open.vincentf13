20f50e7bbd3d730815bb78ec5e858b915c1e35f6
vincentf13 <c.p.kevinf13@gmail.com>
2025-11-27 05:52:07 +0800

    no message

--- sdk-contract/exchange-sdk/exchange-matching-sdk/exchange-matching-sdk-mq/src/main/java/open/vincentf13/exchange/matching/sdk/mq/event/TradeExecutedEvent.java
+++ sdk-contract/exchange-sdk/exchange-matching-sdk/exchange-matching-sdk-mq/src/main/java/open/vincentf13/exchange/matching/sdk/mq/event/TradeExecutedEvent.java
@@ -3,7 +3,7 @@ package open.vincentf13.exchange.matching.sdk.mq.event;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import open.vincentf13.exchange.common.sdk.constants.ValidationConstant;
-import open.vincentf13.exchange.sdk.common.enums.AssetSymbol;
+import open.vincentf13.exchange.common.sdk.enums.AssetSymbol;
 
 import java.math.BigDecimal;
 import java.time.Instant;
