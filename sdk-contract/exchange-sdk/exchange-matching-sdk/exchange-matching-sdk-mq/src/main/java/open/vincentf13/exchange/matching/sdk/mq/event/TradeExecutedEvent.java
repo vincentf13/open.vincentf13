772d5cc57c97f0216abbdd41688d6317cbd674cf
vincentf13 <c.p.kevinf13@gmail.com>
2025-11-29 10:59:20 +0800

    no message

--- sdk-contract/exchange-sdk/exchange-matching-sdk/exchange-matching-sdk-mq/src/main/java/open/vincentf13/exchange/matching/sdk/mq/event/TradeExecutedEvent.java
+++ sdk-contract/exchange-sdk/exchange-matching-sdk/exchange-matching-sdk-mq/src/main/java/open/vincentf13/exchange/matching/sdk/mq/event/TradeExecutedEvent.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import open.vincentf13.exchange.common.sdk.constants.ValidationConstant;
 import open.vincentf13.exchange.common.sdk.enums.AssetSymbol;
+import open.vincentf13.exchange.common.sdk.enums.OrderSide;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -14,6 +15,8 @@ public record TradeExecutedEvent(
         @NotNull Long tradeId,
         @NotNull Long orderId,
         @NotNull Long counterpartyOrderId,
+        @NotNull OrderSide orderSide,
+        @NotNull OrderSide counterpartyOrderSide,
         @NotNull @DecimalMin(value = ValidationConstant.Names.PRICE_MIN) BigDecimal price,
         @NotNull @DecimalMin(value = ValidationConstant.Names.QUANTITY_MIN) BigDecimal quantity,
         @NotNull Long makerUserId,
