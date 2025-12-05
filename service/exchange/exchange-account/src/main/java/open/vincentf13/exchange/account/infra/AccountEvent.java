bbedaccbe716ab3db530dec0635c25c3a6682510
vincentf13 <c.p.kevinf13@gmail.com>
2025-12-05 19:04:21 +0800

    OrderSubmitted

--- service/exchange/exchange-account/src/main/java/open/vincentf13/exchange/account/infra/AccountEvent.java
+++ service/exchange/exchange-account/src/main/java/open/vincentf13/exchange/account/infra/AccountEvent.java
@@ -6,7 +6,8 @@ import open.vincentf13.sdk.core.log.OpenEvent;
  Account 事件枚舉。
  */
 public enum AccountEvent implements OpenEvent {
-    MATCHING_TRADE_PAYLOAD_MISSING("MatchingTradePayloadMissing", "TradeExecuted payload missing");
+    MATCHING_TRADE_PAYLOAD_MISSING("MatchingTradePayloadMissing", "TradeExecuted payload missing"),
+    ORDER_SUBMITTED_PAYLOAD_INVALID("OrderSubmittedPayloadInvalid", "OrderSubmitted payload invalid");
     
     private final String event;
     private final String message;
