cda00a3a52022002123bdb1e471d5e1f02b4e973
vincentf13 <c.p.kevinf13@gmail.com>
2025-12-06 12:02:51 +0800

    no message

--- service/exchange/exchange-account/src/main/java/open/vincentf13/exchange/account/domain/service/AccountTransactionDomainService.java
+++ service/exchange/exchange-account/src/main/java/open/vincentf13/exchange/account/domain/service/AccountTransactionDomainService.java
@@ -8,8 +8,6 @@ import open.vincentf13.exchange.account.domain.model.PlatformAccount;
 import open.vincentf13.exchange.account.domain.model.PlatformJournal;
 import open.vincentf13.exchange.account.domain.model.UserAccount;
 import open.vincentf13.exchange.account.domain.model.UserJournal;
-import open.vincentf13.exchange.account.domain.service.result.AccountDepositResult;
-import open.vincentf13.exchange.account.domain.service.result.AccountWithdrawalResult;
 import open.vincentf13.exchange.account.infra.AccountErrorCode;
 import open.vincentf13.exchange.account.infra.AccountEvent;
 import open.vincentf13.exchange.account.infra.messaging.publisher.TradeSettlementEventPublisher;
@@ -55,6 +53,30 @@ public class AccountTransactionDomainService {
     private final DefaultIdGenerator idGenerator;
     
     public record FundsFreezeResult(AssetSymbol asset, BigDecimal amount) {
+    }
+
+    public record AccountDepositResult(
+            UserAccount userAssetAccount,
+            UserAccount userEquityAccount,
+            PlatformAccount platformAssetAccount,
+            PlatformAccount platformLiabilityAccount,
+            UserJournal userAssetJournal,
+            UserJournal userEquityJournal,
+            PlatformJournal platformAssetJournal,
+            PlatformJournal platformLiabilityJournal
+    ) {
+    }
+
+    public record AccountWithdrawalResult(
+            UserAccount userAssetAccount,
+            UserAccount userEquityAccount,
+            PlatformAccount platformAssetAccount,
+            PlatformAccount platformLiabilityAccount,
+            UserJournal userAssetJournal,
+            UserJournal userEquityJournal,
+            PlatformJournal platformAssetJournal,
+            PlatformJournal platformLiabilityJournal
+    ) {
     }
     
     @Transactional
