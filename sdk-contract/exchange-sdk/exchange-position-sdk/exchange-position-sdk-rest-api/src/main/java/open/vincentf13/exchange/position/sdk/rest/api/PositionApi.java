cdf166f41e2d0e36f888c5582ac9d1aa923fd8fa
vincentf13 <c.p.kevinf13@gmail.com>
2025-12-06 12:47:59 +0800

    no message

--- sdk-contract/exchange-sdk/exchange-position-sdk/exchange-position-sdk-rest-api/src/main/java/open/vincentf13/exchange/position/sdk/rest/api/PositionApi.java
+++ sdk-contract/exchange-sdk/exchange-position-sdk/exchange-position-sdk-rest-api/src/main/java/open/vincentf13/exchange/position/sdk/rest/api/PositionApi.java
@@ -1,7 +1,11 @@
 package open.vincentf13.exchange.position.sdk.rest.api;
 
 import jakarta.validation.Valid;
-import open.vincentf13.exchange.position.sdk.rest.api.dto.*;
+import open.vincentf13.exchange.position.sdk.rest.api.dto.PositionIntentRequest;
+import open.vincentf13.exchange.position.sdk.rest.api.dto.PositionIntentResponse;
+import open.vincentf13.exchange.position.sdk.rest.api.dto.PositionResponse;
+import open.vincentf13.sdk.auth.auth.Jwt;
+import open.vincentf13.sdk.auth.auth.PrivateAPI;
 import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +17,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface PositionApi {
     
     @PostMapping("/intent")
+    @PrivateAPI
     OpenApiResponse<PositionIntentResponse> determineIntent(@Valid @RequestBody PositionIntentRequest request);
     
     @GetMapping("/{userId}/{instrumentId}")
+    @PrivateAPI
+    @Jwt
     OpenApiResponse<PositionResponse> getPosition(@PathVariable("userId") Long userId,
                                                   @PathVariable("instrumentId") Long instrumentId);
 }
