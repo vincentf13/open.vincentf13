02cb5c72e0a3cfd2cb67fe0d9ea093dcb73348a4
vincentf13 <c.p.kevinf13@gmail.com>
2026-01-01 19:33:50 +0800

    no message

--- sdk-contract/exchange-sdk/exchange-market-sdk/exchange-market-sdk-rest-api/src/main/java/open/vincentf13/exchange/market/sdk/rest/api/MarketMaintenanceApi.java
+++ sdk-contract/exchange-sdk/exchange-market-sdk/exchange-market-sdk-rest-api/src/main/java/open/vincentf13/exchange/market/sdk/rest/api/MarketMaintenanceApi.java
@@ -1,6 +1,5 @@
 package open.vincentf13.exchange.market.sdk.rest.api;
 
-import io.swagger.v3.oas.annotations.Operation;
 import open.vincentf13.sdk.auth.auth.PublicAPI;
 import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
--- sdk-contract/exchange-sdk/exchange-risk-sdk/exchange-risk-sdk-rest-api/src/main/java/open/vincentf13/exchange/risk/sdk/rest/api/RiskMaintenanceApi.java
+++ sdk-contract/exchange-sdk/exchange-risk-sdk/exchange-risk-sdk-rest-api/src/main/java/open/vincentf13/exchange/risk/sdk/rest/api/RiskMaintenanceApi.java
@@ -1,8 +1,5 @@
 package open.vincentf13.exchange.risk.sdk.rest.api;
 
-
-package open.vincentf13.exchange.risk.sdk.rest.api;
-
 import open.vincentf13.sdk.auth.auth.PublicAPI;
 import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
