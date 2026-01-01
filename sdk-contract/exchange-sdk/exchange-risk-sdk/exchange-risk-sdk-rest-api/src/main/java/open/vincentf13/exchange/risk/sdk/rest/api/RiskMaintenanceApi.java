a7cc2194be65404447866e673d15e3edfa2a4544
vincentf13 <c.p.kevinf13@gmail.com>
2026-01-01 18:26:08 +0800

    no message

--- sdk-contract/exchange-sdk/exchange-risk-sdk/exchange-risk-sdk-rest-api/src/main/java/open/vincentf13/exchange/risk/sdk/rest/api/RiskMaintenanceApi.java
+++ sdk-contract/exchange-sdk/exchange-risk-sdk/exchange-risk-sdk-rest-api/src/main/java/open/vincentf13/exchange/risk/sdk/rest/api/RiskMaintenanceApi.java
@@ -1,15 +1,12 @@
 package open.vincentf13.exchange.risk.sdk.rest.api;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+
 import open.vincentf13.sdk.auth.auth.PublicAPI;
 import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 public interface RiskMaintenanceApi {
 
-    @Operation(summary = "重新加載風控快取", description = "觸發 InstrumentCache 與 MarkPriceCache 的重新載入")
     @PostMapping("/reload-caches")
     @PublicAPI 
     OpenApiResponse<Void> reloadCaches();
