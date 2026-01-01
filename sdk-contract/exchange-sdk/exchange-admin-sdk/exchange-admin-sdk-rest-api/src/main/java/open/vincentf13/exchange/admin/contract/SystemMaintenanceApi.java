6c42b3337841cb2e198fa85836668ea2434d6591
vincentf13 <c.p.kevinf13@gmail.com>
2026-01-01 17:37:50 +0800

    no message

--- sdk-contract/exchange-sdk/exchange-admin-sdk/exchange-admin-sdk-rest-api/src/main/java/open/vincentf13/exchange/admin/contract/SystemMaintenanceApi.java
+++ sdk-contract/exchange-sdk/exchange-admin-sdk/exchange-admin-sdk-rest-api/src/main/java/open/vincentf13/exchange/admin/contract/SystemMaintenanceApi.java
@@ -1,17 +1,11 @@
 package open.vincentf13.exchange.admin.contract;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import open.vincentf13.sdk.auth.auth.PublicAPI;
 import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Tag(name = "System Maintenance", description = "系統維護接口")
-@RequestMapping("/api/admin/system")
 public interface SystemMaintenanceApi {
 
-    @Operation(summary = "重置系統數據", description = "刪除 Kafka 所有 Topic 數據，並清空指定的業務數據表")
     @PostMapping("/reset-data")
     @PublicAPI
     OpenApiResponse<Void> resetSystemData();
--- service/exchange/exchange-admin/src/main/java/open/vincentf13/exchange/admin/controller/SystemAdminController.java
+++ service/exchange/exchange-admin/src/main/java/open/vincentf13/exchange/admin/controller/SystemAdminController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import open.vincentf13.exchange.admin.contract.SystemMaintenanceApi;
 import open.vincentf13.exchange.admin.service.SystemMaintenanceCommandService;
 import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/admin/system")
 public class SystemAdminController implements SystemMaintenanceApi {
 
     private final SystemMaintenanceCommandService maintenanceService;
