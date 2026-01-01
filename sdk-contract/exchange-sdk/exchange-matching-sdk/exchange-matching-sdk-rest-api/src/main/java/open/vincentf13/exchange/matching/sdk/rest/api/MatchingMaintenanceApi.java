b688c1ebc4fdb5a628746ff1dfa44627cfa5508e
vincentf13 <c.p.kevinf13@gmail.com>
2026-01-01 19:32:09 +0800

    no message

--- sdk-contract/exchange-sdk/exchange-matching-sdk/exchange-matching-sdk-rest-api/src/main/java/open/vincentf13/exchange/matching/sdk/rest/api/MatchingMaintenanceApi.java
+++ sdk-contract/exchange-sdk/exchange-matching-sdk/exchange-matching-sdk-rest-api/src/main/java/open/vincentf13/exchange/matching/sdk/rest/api/MatchingMaintenanceApi.java
@@ -1,3 +1,5 @@
+package open.vincentf13.exchange.matching.sdk.rest.api;
+
 import open.vincentf13.sdk.auth.auth.PublicAPI;
 import open.vincentf13.sdk.spring.mvc.OpenApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
