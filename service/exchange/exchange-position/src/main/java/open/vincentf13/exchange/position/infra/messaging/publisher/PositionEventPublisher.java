35215a8e2e0223c6f456351a90d119015738f7cd
vincentf13 <c.p.kevinf13@gmail.com>
2026-01-04 00:12:06 +0800

    no message

--- service/exchange/exchange-position/src/main/java/open/vincentf13/exchange/position/infra/messaging/publisher/PositionEventPublisher.java
+++ service/exchange/exchange-position/src/main/java/open/vincentf13/exchange/position/infra/messaging/publisher/PositionEventPublisher.java
@@ -2,6 +2,7 @@ package open.vincentf13.exchange.position.infra.messaging.publisher;
 
 import lombok.RequiredArgsConstructor;
 import open.vincentf13.exchange.position.sdk.mq.event.PositionTopics;
+import open.vincentf13.exchange.position.sdk.mq.event.PositionInvalidFillEvent;
 import open.vincentf13.exchange.position.sdk.mq.event.PositionMarginReleasedEvent;
 import open.vincentf13.exchange.position.sdk.mq.event.PositionUpdatedEvent;
 import open.vincentf13.sdk.infra.mysql.mq.outbox.MqOutboxRepository;
@@ -19,5 +20,9 @@ public class PositionEventPublisher {
     
     public void publishMarginReleased(PositionMarginReleasedEvent event) {
         outboxRepository.append(PositionTopics.POSITION_MARGIN_RELEASED.getTopic(), event.tradeId(), event, null);
+    }
+
+    public void publishInvalidFill(PositionInvalidFillEvent event) {
+        outboxRepository.append(PositionTopics.POSITION_INVALID_FILL.getTopic(), event.userId(), event, null);
     }
 }
