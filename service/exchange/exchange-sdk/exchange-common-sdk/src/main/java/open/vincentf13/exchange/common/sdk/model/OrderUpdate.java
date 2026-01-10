edcf30077163268a690bf40f38d546ad7405daf2
vincentf13 <c.p.kevinf13@gmail.com>
2026-01-11 03:58:07 +0800

    no message

--- service/exchange/exchange-sdk/exchange-common-sdk/src/main/java/open/vincentf13/exchange/common/sdk/model/OrderUpdate.java
+++ service/exchange/exchange-sdk/exchange-common-sdk/src/main/java/open/vincentf13/exchange/common/sdk/model/OrderUpdate.java
@@ -2,13 +2,13 @@ package open.vincentf13.exchange.common.sdk.model;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import open.vincentf13.sdk.core.validator.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import open.vincentf13.exchange.common.sdk.constants.ValidationConstant;
 import open.vincentf13.exchange.common.sdk.enums.OrderSide;
+import open.vincentf13.sdk.core.validator.Id;
 
 import java.math.BigDecimal;
 
