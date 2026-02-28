8b10f78e63b670ce0887748ef112943862cd6b19
vincentf13 <c.p.kevinf13@gmail.com>
2026-02-28 15:44:39 +0800

    no message

--- sdk/sdk-algo/src/main/java/open/vincentf13/sdk/algo/string/GreatestCommonDivisorOfStrings.java
+++ sdk/sdk-algo/src/main/java/open/vincentf13/sdk/algo/string/GreatestCommonDivisorOfStrings.java
@@ -15,7 +15,19 @@ import lombok.experimental.UtilityClass;
 public class GreatestCommonDivisorOfStrings {
 
     public static String gcdOfStrings(String str1, String str2) {
-        // TODO: 請在此處實作演算法邏輯
-        return "";
+        // 若兩個字串正反拼接不相等，代表沒有共同的公因數字串
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        
+        // 取得兩字串長度的最大公因數
+        int gcdLength = gcd(str1.length(), str2.length());
+        
+        // 擷取最大公因數長度的子字串
+        return str1.substring(0, gcdLength);
+    }
+
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
