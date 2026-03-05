a07d53d45ca66c0db2674c22cbd1000917e53a8d
vincentf13 <c.p.kevinf13@gmail.com>
2026-03-06 00:03:29 +0800

    no message

--- sdk/sdk-algo/src/test/java/open/vincentf13/sdk/algo/greedy/CanPlaceFlowersTest.java
+++ sdk/sdk-algo/src/test/java/open/vincentf13/sdk/algo/greedy/CanPlaceFlowersTest.java
@@ -1,6 +1,5 @@
 package open.vincentf13.sdk.algo.greedy;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -8,17 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 class CanPlaceFlowersTest {
 
     @Test
-    @DisplayName("測試所有種花案例")
     void testCanPlaceFlowers() {
+        for (int i = 1; i <= 10; i++) {
+            assertTrue(CanPlaceFlowers.canPlaceFlowers(new int[i], (i + 1) / 2));
+            assertFalse(CanPlaceFlowers.canPlaceFlowers(new int[i], (i + 1) / 2 + 1));
+        }
+
         assertTrue(CanPlaceFlowers.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 1));
         assertFalse(CanPlaceFlowers.canPlaceFlowers(new int[]{1, 0, 0, 0, 1}, 2));
         assertTrue(CanPlaceFlowers.canPlaceFlowers(new int[]{0, 0, 1, 0, 1}, 1));
         assertTrue(CanPlaceFlowers.canPlaceFlowers(new int[]{1, 0, 1, 0, 0}, 1));
-        assertTrue(CanPlaceFlowers.canPlaceFlowers(new int[]{0}, 0));
-        assertTrue(CanPlaceFlowers.canPlaceFlowers(new int[]{0}, 1));
-        assertFalse(CanPlaceFlowers.canPlaceFlowers(new int[]{0}, 2));
-        assertTrue(CanPlaceFlowers.canPlaceFlowers(new int[]{1}, 0));
-        assertFalse(CanPlaceFlowers.canPlaceFlowers(new int[]{1}, 1));
-        assertFalse(CanPlaceFlowers.canPlaceFlowers(new int[]{1}, 2));
+        assertFalse(CanPlaceFlowers.canPlaceFlowers(new int[]{0, 1, 0}, 1));
     }
 }
